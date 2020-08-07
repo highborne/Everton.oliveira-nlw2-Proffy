e2635f27f8d5c9996e35b7af76f8cc82c539386c
highborne <evertonluizcoster@gmail.com>
2020-08-06 22:41:38 -0300

    Correção do CORS

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -31,17 +31,14 @@ export default class ClassesController{
                     .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
                     .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
                     .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
-                    .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
-
-                    
+                    .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])                  
                 
             })
             .where('classes.subject', subject)
             .join('users', 'classes.user_id', '=', 'users.id')
             .select(['classes.*', 'users.*']);
-
-
-        return response.json(classes);
+   
+            return response.json(classes);
     }
 
     async create(request: Request, response: Response) {
--- web/src/ components/TeacherItem/index.tsx
+++ web/src/ components/TeacherItem/index.tsx
@@ -3,35 +3,49 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem(){
+export interface Teacher{
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps{
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+    function creatNewConnection(){
+        api.post('connections', {
+            user_id: teacher.id,
+        });
+    }
     return(
         <article className="teacher-item">
         <header>
-            <img src="https://pbs.twimg.com/profile_images/1156684597045936135/_YxF_DY4_400x400.jpg"
-            alt="Everton Cardoso"/>
+            <img src={teacher.avatar} alt={teacher.name} />
             <div>
-                <strong>Everton Cardoso</strong>
-                <span>Matematica</span>
-               
+                <strong>{teacher.name}</strong>
+                <span>{teacher.subject}</span>
             </div>
         </header>
-
         <p>
-            Testo auqi e aquir tbm
-            <br /><br />
-            dasdasa alooeiaoklkasdklkklanqmndaksdkla
+            {teacher.bio}
         </p>
-
         <footer>
             <p>
                 Preço/hora
-                <strong>R$ 80,00</strong>
+                <strong>{teacher.cost}</strong>
             </p>
-            <button type="button">
+            <a onClick={creatNewConnection} href={`https://wa.me/${teacher.whatsapp}`} target="blank">
                 <img src={whatsappIcon} alt="Whatsapp"/>
                 Entrar em contato
-            </button>
+            </a>
         </footer>
     </article>
     );
